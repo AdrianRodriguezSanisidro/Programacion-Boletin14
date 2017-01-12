1d798b750c43d1268002c26761b4e87eea7f6b99
Adry <Adry@Sanisidro>
2017-01-12 12:41:00 +0100

    Constructores por defecto y sin serlo

--- src/boletin/pkg14/TemperaturaErradaExcepcion.java
+++ src/boletin/pkg14/TemperaturaErradaExcepcion.java
@@ -4,6 +4,11 @@ package boletin.pkg14;
  *
  * @author Adry
  */
-public class TemperaturaErradaExcepcion {
-    
+public class TemperaturaErradaExcepcion extends Exception{
+    public TemperaturaErradaExcepcion(){
+        super("A temperatura debe ser a 80º");
+    }
+    public TemperaturaErradaExcepcion(String t){
+        super (t);
+    }
 }
