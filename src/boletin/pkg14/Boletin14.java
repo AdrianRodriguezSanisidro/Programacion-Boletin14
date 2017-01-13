7fe65ecdc01dda870c68e0cf3aa4797e3c25f235
Adry <Adry@Sanisidro>
2017-01-13 12:03:30 +0100

    Final

--- src/boletin/pkg14/Boletin14.java
+++ src/boletin/pkg14/Boletin14.java
@@ -7,7 +7,13 @@ package boletin.pkg14;
 public class Boletin14 {
 
     public static void main(String[] args) {
-        
+        ConversorTemperaturas obx1=new ConversorTemperaturas();
+        try{
+            obx1.centigradosAFharenheit(85);
+            obx1.centigradosAReamur(98);
+        }catch(TemperaturaErradaExcepcion s){
+            System.out.println(s.toString());
+        }
     }
     
 }
