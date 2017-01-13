9cad3fa93fe78cf07c357d131a1f5e02c6b2c007
Adry <Adry@Sanisidro>
2017-01-13 11:55:51 +0100

    Metodo de convertir los centigrados a reamur

--- src/boletin/pkg14/ConversorTemperaturas.java
+++ src/boletin/pkg14/ConversorTemperaturas.java
@@ -6,13 +6,21 @@ package boletin.pkg14;
  */
 public class ConversorTemperaturas {
     final float centigrados=80;
-    float fharenheit;
+    float fharenheit,reamur;
     public float centigradosAFharenheit(float centigrados)throws TemperaturaErradaExcepcion{
         if(centigrados>=80)
             throw new TemperaturaErradaExcepcion();
         else{
            fharenheit=9.0f/5.0f*centigrados+32.4f;
-            System.out.println("La temperatura es de "+fharenheit+"grados fharenheit");}
+            System.out.println("La temperatura es de "+fharenheit+" grados fharenheit");}
         return fharenheit;
 }
+    public void centigradosAReamur(float centigrados)throws TemperaturaErradaExcepcion{
+        if(centigrados>=80)
+            throw new TemperaturaErradaExcepcion();
+        else{
+            reamur=4.0f/5.0f*centigrados;
+            System.out.println("La temperatura es de "+reamur+" grados reamur");
+        }
+    }
 }
