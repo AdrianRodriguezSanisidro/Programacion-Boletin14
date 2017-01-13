b965d933271afcdf3ec362d1294dafb9334e5866
Adry <Adry@Sanisidro>
2017-01-13 12:01:48 +0100

    Cambiar nombre de variable y corregir sentido del sibolo ><

--- src/boletin/pkg14/ConversorTemperaturas.java
+++ src/boletin/pkg14/ConversorTemperaturas.java
@@ -5,10 +5,10 @@ package boletin.pkg14;
  * @author Adry
  */
 public class ConversorTemperaturas {
-    final float centigrados=80;
+    final float min=80;
     float fharenheit,reamur;
     public float centigradosAFharenheit(float centigrados)throws TemperaturaErradaExcepcion{
-        if(centigrados>=80)
+        if(centigrados<=min)
             throw new TemperaturaErradaExcepcion();
         else{
            fharenheit=9.0f/5.0f*centigrados+32.4f;
@@ -16,7 +16,7 @@ public class ConversorTemperaturas {
         return fharenheit;
 }
     public void centigradosAReamur(float centigrados)throws TemperaturaErradaExcepcion{
-        if(centigrados>=80)
+        if(centigrados<=min)
             throw new TemperaturaErradaExcepcion();
         else{
             reamur=4.0f/5.0f*centigrados;
