b19f435a687fc8b1b19c193e922bd4443dc37a67
Adry <Adry@Sanisidro>
2017-01-13 11:47:45 +0100

    Metodo de convertir los centigrados a fahrenheit

--- src/boletin/pkg14/ConversorTemperaturas.java
+++ src/boletin/pkg14/ConversorTemperaturas.java
@@ -5,5 +5,14 @@ package boletin.pkg14;
  * @author Adry
  */
 public class ConversorTemperaturas {
-    
+    final float centigrados=80;
+    float fharenheit;
+    public float centigradosAFharenheit(float centigrados)throws TemperaturaErradaExcepcion{
+        if(centigrados>=80)
+            throw new TemperaturaErradaExcepcion();
+        else{
+           fharenheit=9.0f/5.0f*centigrados+32.4f;
+            System.out.println("La temperatura es de "+fharenheit+"grados fharenheit");}
+        return fharenheit;
+}
 }
